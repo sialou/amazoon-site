0e41569ce28bdfa7fcbb8dbf3ae1e768bb480779
sialou <floraskouassi@hotmail.fr>
2026-03-02 16:35:40 +0000

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // <-- INDISPENSABLE pour [disabled] et *ngIf
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule], // <-- AJOUTÉ ICI
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  currentYear = new Date().getFullYear(); // <-- utiliser dans le HTML
+  isMenuOpen = false;
+  isReady = false;
+  currentYear = new Date().getFullYear();
+
+ 
+
+
+
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule, CommonModule, TranslateModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomePageComponent {
+export class HomePageComponent implements AfterViewInit {
 
+  @ViewChild('impactSection') impactSection!: ElementRef;
+
+  count1 = 0;
+  count2 = 0;
+  count3 = 0;
+  count4 = 0;
+
+  private animated = false;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting && !this.animated) {
+          this.startCounters();
+          this.animated = true;
+        }
+      });
+    }, { threshold: 0.4 });
+
+    observer.observe(this.impactSection.nativeElement);
+  }
+
+  startCounters() {
+    this.animateValue('count1', 30000, 2000);
+    this.animateValue('count2', 40, 1500);
+    this.animateValue('count3', 10, 1500);
+    this.animateValue('count4', 100, 2000);
+  }
+
+  animateValue(property: 'count1' | 'count2' | 'count3' | 'count4', end: number, duration: number) {
+    const startTime = performance.now();
+
+    const animate = (currentTime: number) => {
+      const progress = Math.min((currentTime - startTime) / duration, 1);
+      this[property] = Math.floor(progress * end);
+
+      if (progress < 1) {
+        requestAnimationFrame(animate);
+      } else {
+        this[property] = end;
+      }
+    };
+
+    requestAnimationFrame(animate);
+  }
 }
